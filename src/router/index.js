05c3771d1edec9c2faf7566f07a5f7bcca9b7299
이동하 <siganshoyou@gmail.com>
2023-01-31 15:39:46 +0900

    success

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: 'start',
+    component: () => import("@/views/StartView.vue")
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/LoginView.vue")
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: () => import("@/views/SearchView.vue")
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/HomeView.vue")
+}
 ]
 
 const router = createRouter({
